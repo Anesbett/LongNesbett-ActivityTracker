8bc7c5920f940c9464a7adb6bda1539999b97fe7
Chandler Long <chandlertlong@gmail.com>
2020-02-22 15:13:19 -0500

    Updated Documentation

--- LongNesbett-ActivityTracker/src/ActivityTracker/ActivityTracker.java
+++ LongNesbett-ActivityTracker/src/ActivityTracker/ActivityTracker.java
@@ -3,29 +3,35 @@ package ActivityTracker;
 public class ActivityTracker {
     public static void main(String[] args) {
 
-        //fetch student record based on his roll no from the database
+        // Retrieve users stats based on values assigned
         User model  = retriveUserInformation();
 
-        //Create a view : to write student details on console
+        // Create a view to write user details on scene
         HomeView view = new HomeView();
 
         HomeController controller = new HomeController(model, view);
 
+        // The controllers view must be updated in order to reflect the changes
         controller.updateView();
 
-        //update model data
+        // Update the models data
         //controller.setUsersName("John");
 
         //controller.updateView();
     }
 
+
+    /**
+     * This functon assigns values to the users in order to show the flow of our MVC Pattern
+     * @return  User  the function returns an object User with the assigned values
+     */
     private static User retriveUserInformation(){
         User user = new User();
         user.setUsersName("Robert");
         user.setUsersAge(27);
         user.setUsersWeight(225);
         user.setUsersHeight(78);
-        user.setUsersGoal("I want to loose all of my fat.");
+        user.setUsersGoal("I want to stop being a fat boy.");
         return user;
     }
 }
--- LongNesbett-ActivityTracker/src/ActivityTracker/HomeView.java
+++ LongNesbett-ActivityTracker/src/ActivityTracker/HomeView.java
@@ -1,6 +1,9 @@
 package ActivityTracker;
 
 public class HomeView {
+
+    // The view will be where we assign out users values to objects
+
     public void printUserDetails(String userName, Integer userAge, Integer userWeight, Integer userHeight, String userGoal){
         System.out.println("Name: " + userName);
         System.out.println("User Age: " + userAge);
