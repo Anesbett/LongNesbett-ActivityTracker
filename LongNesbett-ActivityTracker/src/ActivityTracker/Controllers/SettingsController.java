1e40d54587faed27b0b54099d02db7540566d3b3
Chandler Long <chandlertlong@gmail.com>
2020-03-27 13:06:06 -0400

    Status 200 WeatherData Request

--- LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/SettingsController.java
+++ LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/SettingsController.java
@@ -2,18 +2,11 @@ package ActivityTracker.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import ActivityTracker.Custom.Layout;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
 
 public class SettingsController implements Initializable {
 
@@ -22,21 +15,8 @@ public class SettingsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    setUpView();
+        Layout.setUpView(Settings);
         System.out.println("You have made it to the settings controller");
-
-    }
-
-
-    public void setUpView() {
-
-        // Apply the gradient to the background
-        Stop[] stops = { new Stop(0, Color.rgb(45,45,45)), new Stop(1, Color.rgb(8,8,8))};
-        LinearGradient lg = new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
-
-        BackgroundFill bgFill = new BackgroundFill(lg, CornerRadii.EMPTY, Insets.EMPTY);
-        Settings.setBackground(new Background(bgFill));
-
 
     }
 }
