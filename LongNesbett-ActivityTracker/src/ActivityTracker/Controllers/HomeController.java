65c93923fa1cc76701003628b8c361a02b0f8a83
Andrew <nesbettar@g.cofc.edu>
2020-03-31 14:35:15 -0400

    Adding clock functionality

--- LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/HomeController.java
+++ LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/HomeController.java
@@ -1,23 +1,15 @@
 package ActivityTracker.Controllers;
 
-import java.awt.*;
+import ActivityTracker.Extensions.Layout;
 import java.net.URL;
 import java.util.ResourceBundle;
 import ActivityTracker.Models.Clock;
 
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.Stop;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.LinearGradient;
 import javafx.scene.control.Label;
+
 
 
 public class HomeController implements Initializable {
@@ -35,30 +27,9 @@ public class HomeController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setUpView();
+        Layout.setUpView(Home);
         clockLabel.setText(time);
         System.out.println("You have made it to the home controller");
 
-
     }
-
-
-    public void setUpView() {
-
-        // Apply the gradient to the background
-        Stop[] stops = { new Stop(0, Color.rgb(45,45,45)), new Stop(1, Color.rgb(8,8,8))};
-        LinearGradient lg = new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
-
-        BackgroundFill bgFill = new BackgroundFill(lg, CornerRadii.EMPTY, Insets.EMPTY);
-        Home.setBackground(new Background(bgFill));
-
-
-
-
-
-    }
-
-
-
-
 }
--- LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/ProfileController.java
+++ LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/ProfileController.java
@@ -2,18 +2,12 @@ package ActivityTracker.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import ActivityTracker.Extensions.Layout;
+
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
 
 public class ProfileController implements Initializable {
 
@@ -23,22 +17,8 @@ public class ProfileController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setUpView();
+        Layout.setUpView(Profile);
         System.out.println("You have made it to the profile controller");
-
-    }
-
-    public void setUpView() {
-
-        // Apply the gradient to the background
-        Stop[] stops = { new Stop(0, Color.rgb(45,45,45)), new Stop(1, Color.rgb(8,8,8))};
-        LinearGradient lg = new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
-
-        BackgroundFill bgFill = new BackgroundFill(lg, CornerRadii.EMPTY, Insets.EMPTY);
-        Profile.setBackground(new Background(bgFill));
-
-
-
 
     }
 }
