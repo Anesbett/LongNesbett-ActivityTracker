c886def0a7b2fffec5ccfccf69c4a0600cc49b17
Andrew <nesbettar@g.cofc.edu>
2020-03-31 14:35:53 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	LongNesbett-ActivityTracker/LongNesbett-ActivityTracker.iml
    #	LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/HomeController.java
    #	LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/SettingsController.java

---
+++ LongNesbett-ActivityTracker/src/ActivityTracker/Extensions/Layout.java
@@ -0,0 +1,26 @@
+package ActivityTracker.Extensions;
+
+import javafx.scene.layout.Pane;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+
+public class Layout {
+
+    public static void setUpView(Pane view) {
+
+        // Apply the gradient to the background
+        Stop[] stops = { new Stop(0, Color.rgb(45,45,45)), new Stop(1, Color.rgb(8,8,8))};
+        LinearGradient lg = new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
+
+        BackgroundFill bgFill = new BackgroundFill(lg, CornerRadii.EMPTY, Insets.EMPTY);
+        view.setBackground(new Background(bgFill));
+
+    }
+}
---
+++ LongNesbett-ActivityTracker/src/ActivityTracker/Extensions/WeatherData.java
@@ -0,0 +1,73 @@
+package ActivityTracker.Extensions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class WeatherData {
+    private static HttpURLConnection connection;
+
+    public static void getWeatherData() {
+        // Try to connect to the API
+
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+
+        try {
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?zip=29566,us&appid=0f965a23a25240372a5631d7432a7079");
+            connection = (HttpURLConnection) url.openConnection();
+
+            // Request Setup
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status = connection.getResponseCode();
+            //System.out.println(status);
+
+            // If the connection error has a problem
+            if (status > 299) {
+                reader = new BufferedReader((new InputStreamReader(connection.getErrorStream())));
+                while((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            } else {
+                // The connection was successful
+                reader = new BufferedReader((new InputStreamReader(connection.getInputStream())));
+                while((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            System.out.println(responseContent.toString());
+            //parse(responseContent.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+
+    public static String parse(String responseBody) {
+        JSONArray forecasts = new JSONArray(responseBody);
+        for (int i = 0; i < forecasts.length(); i++) {
+            JSONObject forecast = forecasts.getJSONObject(i);
+            int weatherType = forecast.getInt("weather");
+            System.out.println(weatherType);
+        }
+        return null;
+    }
+
+}
