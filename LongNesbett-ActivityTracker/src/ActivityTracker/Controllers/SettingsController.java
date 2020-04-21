59851f23e59c2bd35e2b2f697e2ce057b87e5eb2
Andrew <nesbettar@g.cofc.edu>
2020-04-21 16:51:29 -0400

    Adding input validation to Settings page

--- LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/SettingsController.java
+++ LongNesbett-ActivityTracker/src/ActivityTracker/Controllers/SettingsController.java
@@ -28,7 +28,6 @@ public class SettingsController implements Initializable {
     @FXML
     private TextField goalTextField;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Layout.setUpView(Settings);
@@ -40,50 +39,114 @@ public class SettingsController implements Initializable {
         goalTextField.setText(ActivityTracker.user.getUsersGoal());
 
     }
-
+    
+    /**
+     * Method that updates goal text
+     */
     @FXML
     private void updateGoal(){
         ActivityTracker.user.setUsersGoal(goalTextField.getText());
     }
 
+    /**
+     * Method that subtracts 1 minute to current time and updates text
+     */
     @FXML
     private void clockLeft(){
         ActivityTracker.clock.minusOne();
         clockLabel.setText(ActivityTracker.clock.getTime());
     }
+
+    /**
+     * Method that adds 1 minute to current time and updates text
+     */
     @FXML
     private void clockRight(){
         ActivityTracker.clock.addOne();
         clockLabel.setText(ActivityTracker.clock.getTime());
     }
+
+    /**
+     * Method that subtracts 1 to current user's age and updates text
+     * Input validation requires input to be greater than 0
+     */
     @FXML
     private void ageLeft(){
-        ActivityTracker.user.setUsersAge(ActivityTracker.user.getUsersAge() - 1);
-        ageLabel.setText(ActivityTracker.user.getUsersAge().toString());
+        if (ActivityTracker.user.getUsersAge() <= 0){
+            ageLabel.setText("0");
+        }
+        else {
+            ActivityTracker.user.setUsersAge(ActivityTracker.user.getUsersAge() - 1);
+            ageLabel.setText(ActivityTracker.user.getUsersAge().toString());
+        }
     }
+
+    /**
+     * Method that adds 1 to current user's age and updates text
+     * Input validation requires input to be less than or equal to 116
+     */
     @FXML
     private void ageRight(){
-        ActivityTracker.user.setUsersAge(ActivityTracker.user.getUsersAge() + 1);
-        ageLabel.setText(ActivityTracker.user.getUsersAge().toString());
+        if (ActivityTracker.user.getUsersAge() >= 116){
+            ageLabel.setText("116");
+        }
+        else {
+            ActivityTracker.user.setUsersAge(ActivityTracker.user.getUsersAge() + 1);
+            ageLabel.setText(ActivityTracker.user.getUsersAge().toString());
+        }
     }
+
+    /**
+     * Method that adds 1 to current user's weight and updates text
+     */
     @FXML
     private void weightRight(){
         ActivityTracker.user.setUsersWeight(ActivityTracker.user.getUsersWeight() + 1);
         weightLabel.setText(ActivityTracker.user.getUsersWeight().toString());
     }
+
+    /**
+     * Method that subtracts 1 to current user's weight and updates text
+     * Input validation requires input to be greater than -1 pounds
+     */
     @FXML
     private void weightLeft(){
-        ActivityTracker.user.setUsersWeight(ActivityTracker.user.getUsersWeight() - 1);
-        weightLabel.setText(ActivityTracker.user.getUsersWeight().toString());
+        if (ActivityTracker.user.getUsersWeight() <= 0){
+            weightLabel.setText("0");
+        }
+        else {
+            ActivityTracker.user.setUsersWeight(ActivityTracker.user.getUsersWeight() - 1);
+            weightLabel.setText(ActivityTracker.user.getUsersWeight().toString());
+        }
     }
+
+    /**
+     * Method that subtracts 1 to current user's height and updates text
+     * input validation requires input to be greater than 0 inches tall
+     */
     @FXML
     private void heightLeft(){
-        ActivityTracker.user.setUsersHeight(ActivityTracker.user.getUsersHeight() - 1);
-        heightLabel.setText(ActivityTracker.user.getUsersHeight().toString());
+        if (ActivityTracker.user.getUsersHeight() <= 1){
+            heightLabel.setText("1");
+        }
+        else {
+            ActivityTracker.user.setUsersHeight(ActivityTracker.user.getUsersHeight() - 1);
+            heightLabel.setText(ActivityTracker.user.getUsersHeight().toString());
+        }
     }
+
+    /**
+     * Method that adds 1 to current user's height and updates text
+     * Input validation requires input to be less than 108 inches tall
+     */
     @FXML
     private void heightRight(){
-        ActivityTracker.user.setUsersHeight(ActivityTracker.user.getUsersHeight() + 1);
-        heightLabel.setText(ActivityTracker.user.getUsersHeight().toString());
+        if (ActivityTracker.user.getUsersHeight() >= 108){
+            heightLabel.setText("108");
+        }
+        else {
+            ActivityTracker.user.setUsersHeight(ActivityTracker.user.getUsersHeight() + 1);
+            heightLabel.setText(ActivityTracker.user.getUsersHeight().toString());
+        }
     }
 }
